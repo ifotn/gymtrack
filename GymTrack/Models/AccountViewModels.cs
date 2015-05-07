1911d38d1a4d71b003c59d557a51cb5c7b4e7497
Jaret Wright <jaret.wright@georgiancollege.ca>
2015-05-07 15:56:41 -0400

    Removed the comments as JW

--- GymTrack/Models/AccountViewModels.cs
+++ GymTrack/Models/AccountViewModels.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
-/* Does this show the name of Jaret the Great?*/
 namespace GymTrack.Models
 {
     public class ExternalLoginConfirmationViewModel
