86da6a2c68401acee79a17cc77ae830e6ef25fb0
Jaret Wright <jaret.wright@georgiancollege.ca>
2015-05-21 14:51:54 -0400

    Here is a updated ExerciseDayProgramController.cs

--- GymTrack/Controllers/ExerciseDayProgramsController.cs
+++ GymTrack/Controllers/ExerciseDayProgramsController.cs
@@ -19,7 +19,7 @@ namespace GymTrack.Controllers
         // GET: ExerciseDayPrograms
         public ActionResult Index()
         {
-            /* ha ha can you see this change?*/
+            /* ha ha can you see this change?*/ 
             return View(db.ExerciseDayPrograms.ToList());
         }
 
