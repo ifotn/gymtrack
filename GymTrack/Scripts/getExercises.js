3fcb2ad66015afe2c104a9e0162ff53fd976412e
Jaret Wright <jaret.wright@georgiancollege.ca>
2015-06-15 11:04:02 -0400

    Updated the planned reps and sets create to show existing exercises in the routine.

---
+++ GymTrack/Scripts/getExercises.js
@@ -0,0 +1,8 @@
+﻿
+    function getExerciseID() {
+        if ($("#programID").val() != "") {
+
+            var url = '/PlannedRepsAndSets/Create/' + $("#programID").val();
+            $(location).attr('href', url);
+        }
+    };
